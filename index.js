112162943bea6dacbd523e716a9f4621c5beea68
Christian Sany <christian@sany.ch>
2022-05-10 18:53:45 +0200

    Add 01 html css basics lecture

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const fs = require('fs');
-const { mdToPdf } = require('md-to-pdf');
+const fs = require("fs");
+const { mdToPdf } = require("md-to-pdf");
 
 const mdToPdfProxy = async (from, to) => {
-  if(!fs.existsSync(to)) {
-    fs.writeFileSync(to, "")
+  if (!fs.existsSync(to)) {
+    fs.writeFileSync(to, "");
   }
 
-  await mdToPdf(
-    { path: from },
-    { dest: to }
-  ).catch(console.error);
-}
- 
+  await mdToPdf({ path: from }, { dest: to }).catch(console.error);
+};
+
 (async () => {
   // 00_HTML_CSS_Vorkurs
-  await mdToPdfProxy('./00_HTML_CSS_Vorkurs/was-ist-html.md', './00_HTML_CSS_Vorkurs/dist/was-ist-html.pdf')
-  await mdToPdfProxy('./00_HTML_CSS_Vorkurs/was-ist-css.md', './00_HTML_CSS_Vorkurs/dist/was-ist-css.pdf')
+  await mdToPdfProxy(
+    "./00_HTML_CSS_Vorkurs/was-ist-html.md",
+    "./00_HTML_CSS_Vorkurs/dist/was-ist-html.pdf"
+  );
+  await mdToPdfProxy(
+    "./00_HTML_CSS_Vorkurs/was-ist-css.md",
+    "./00_HTML_CSS_Vorkurs/dist/was-ist-css.pdf"
+  );
+
+  // 01_HTML_CSS_Basics
+  await mdToPdfProxy(
+    "./01_HTML_CSS_Basics/fortgeschrittene-basics-html.md",
+    "./01_HTML_CSS_Basics/dist/fortgeschrittene-basics-html.pdf"
+  );
+  await mdToPdfProxy(
+    "./01_HTML_CSS_Basics/fortgeschrittene-basics-css.md",
+    "./01_HTML_CSS_Basics/dist/fortgeschrittene-basics-css.pdf"
+  );
 })();
